e4a912a686a83759d8e80ada55a411e961a9e632
saurabh1144 <saurabhchouragade706@gmail.com>
2024-11-28 20:52:15 +0530

    about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./About.css";
+import { FaGraduationCap } from "react-icons/fa6";
+import myimg from '../../assets/abc.png'
+
 
 const About = () => {
   return (
@@ -10,33 +13,24 @@ const About = () => {
         skills mostly in terms of programming and technology
       </span>
       <div className="row">
-        <div className="col-6">
+        <div className="col-lg-5 col-md-6 col-sm-12">
           <div className="aboutintro">
-            <h2 className="know">Know About Me!</h2>
-            <p className="aboutpara">
-              Greetings! I'm Saurabh Chouragade, a dedicated software engineer
-              located in India. My expertise spans TypeScript, JavaScript,
-              Node.js, React.js, and Next.js, encompassing both frontend and
-              backend development.
-            </p>
-            <p className="aboutpara">
-              I have a proven track record of crafting high-quality digital
-              solutions, from creating seamless user interfaces with React.js to
-              architecting robust backend systems using Node.js. My focus is
-              always on delivering clean, efficient, and scalable code that
-              exceeds expectations.
-            </p>
-            <p className="aboutpara">
-              Throughout my career, I've had the opportunity to work on a
-              variety of projects, each presenting unique challenges and
-              opportunities for growth. I thrive on the continuous learning and
-              problem-solving inherent in the tech industry.
-            </p>
+          <img src={myimg} alt="myimg" className="image"/>
+         
+              
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-lg-7 col-md-6 col-sm-12">
         <div className="aboutskill">
-        <h2>About Skill</h2>
+        <p className="aboutpara">Greetings! I'm <b>Saurabh Chouragade</b>, a dedicated software engineer located in India. My expertise spans TypeScript, JavaScript, Node.js, React.js, and Next.js, encompassing both frontend and backend development.</p>
+        <h4>Education</h4>
+        <ul className="education">
+            <li className="listitem"><FaGraduationCap/> Master Of Computer Applications(MCA) 2023</li>
+                 <label className="collegepg">Shree RamdeoBaba College of Engineering and Management Nagpur (RCOEM)</label>
+                 <li className="listitem"><FaGraduationCap/> Bachelors Of Science (Computer Science) 2019</li>
+                 <label className="collegepg">Dhote Bhandhu Science College Gondia (M.H)</label>
+        </ul>
+
 
         </div>
         
--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,49 +1,57 @@
-import React, { useEffect } from 'react'
-import './intro.css'
-import { Link } from 'react-scroll'
-import my from '../../assets/myimg.png'
-import down from '../../assets/downlaod.png'
+import React, { useEffect } from "react";
+import "./intro.css";
+import { Link } from "react-scroll";
+
+import down from "../../assets/downlaod.png";
+import banner from "../../assets/undraw.svg"
+
 
 const Intro = () => {
-  useEffect(()=>{
-    var app = document.getElementById('app');
+  useEffect(() => {
+    var app = document.getElementById("app");
 
     var typewriter = new Typewriter(app, {
-        loop: true
+      loop: true,
     });
-    
-    typewriter.typeString('Web Developer')
-        .pauseFor(2000)
-        .deleteAll()
-        .typeString('React Js')
-        .pauseFor(2000)
-        .deleteAll()
-        .typeString('<strong>FrontEnd Developer</strong>')
-        .pauseFor(2000)
-        .start();
-  },[])
- 
+
+    typewriter
+      .typeString("Web Developer")
+      .pauseFor(2000)
+      .deleteAll()
+      .typeString("React Js")
+      .pauseFor(2000)
+      .deleteAll()
+      .typeString("<strong>FrontEnd Developer</strong>")
+      .pauseFor(2000)
+      .start();
+  }, []);
+
   return (
-    <section className='intro'>
-   
-   
-   
-      
-
-    <div className='introContent'>
-        <span className='hello'>Hello,</span>
-        <span className='introText'> I'm
-        <span className='introName'> Saurabh </span><br/><span id='app'>Frontend Developer</span>
-        <p className='introPara'>I am a skilled Web Developer with experience in creating <br/>visually appealing and user friendly websites.</p>
-
-        <Link> <button className='btn'><img src={down} alt='icon' className='icon'/>Resume</button></Link>
+    <section className="intro">
+      <div className="introContent">
+        <span className="hello">Hello,</span>
+        <span className="introText">
+          {" "}
+          I'm
+          <span className="introName"> Saurabh </span>
+          <br />
+          <span id="app">Frontend Developer</span>
+          <p className="introPara">
+            I am a skilled Web Developer with experience in creating <br />
+            visually appealing and user friendly websites.
+          </p>
+          <Link>
+            {" "}
+            <button className="btn">
+              <img src={down} alt="icon" className="icon" />
+              Resume
+            </button>
+          </Link>
         </span>
-    </div>
-    <img src={my} alt='img' className='myImg'/>
-
-  
+      </div>
+      <img src={banner} alt="img" className="myImg" />
     </section>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
